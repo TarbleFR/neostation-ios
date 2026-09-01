82e39ac2a5cc33a40edda5f4659a284928f8dc77
Tarble FR <f9wynyv9qy@privaterelay.appleid.com>
2026-09-01 16:50:26 +0200

    ui: extend achievements panel toward footer

--- lib/screens/game_screen/game_details_card/tabs/game_details_achievements_tab.dart
+++ lib/screens/game_screen/game_details_card/tabs/game_details_achievements_tab.dart
@@ -27,7 +27,7 @@ class GameDetailsAchievementsTab extends StatefulWidget {
     required this.isLoading,
     required this.onRefresh,
     this.topOffset = 55.0,
-    this.bottomOffset = 110.0,
+    this.bottomOffset = 78.0,
     this.leftOffset = 12.0,
     this.rightOffset = 12.0,
     this.headerAction,
