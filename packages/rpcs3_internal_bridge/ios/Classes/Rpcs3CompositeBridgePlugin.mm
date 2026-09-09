c37f1c2c3a59ea15f422b457f03020398678df2c
Tarble FR <f9wynyv9qy@privaterelay.appleid.com>
2026-09-09 23:20:46 +0200

    feat(rpcs3): register runtime tuning bridge

--- packages/rpcs3_internal_bridge/ios/Classes/Rpcs3CompositeBridgePlugin.mm
+++ packages/rpcs3_internal_bridge/ios/Classes/Rpcs3CompositeBridgePlugin.mm
@@ -1,12 +1,14 @@
 #import "Rpcs3CompositeBridgePlugin.h"
 #import "Rpcs3InternalBridgePlugin.h"
 #import "Rpcs3JitBridgePlugin.h"
+#import "Rpcs3RuntimeTuningPlugin.h"
 
 @implementation Rpcs3CompositeBridgePlugin
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [Rpcs3InternalBridgePlugin registerWithRegistrar:registrar];
   [Rpcs3JitBridgePlugin registerWithRegistrar:registrar];
+  [Rpcs3RuntimeTuningPlugin registerWithRegistrar:registrar];
 }
 
 @end
