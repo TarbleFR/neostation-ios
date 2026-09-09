ab3ea4ef1f71a08ce6e5a4fde8610924538a0298
Tarble FR <f9wynyv9qy@privaterelay.appleid.com>
2026-09-09 23:22:59 +0200

    feat(rpcs3): register document picker bridge

--- packages/rpcs3_internal_bridge/ios/Classes/Rpcs3CompositeBridgePlugin.mm
+++ packages/rpcs3_internal_bridge/ios/Classes/Rpcs3CompositeBridgePlugin.mm
@@ -1,4 +1,5 @@
 #import "Rpcs3CompositeBridgePlugin.h"
+#import "Rpcs3DocumentPickerPlugin.h"
 #import "Rpcs3InternalBridgePlugin.h"
 #import "Rpcs3JitBridgePlugin.h"
 #import "Rpcs3RuntimeTuningPlugin.h"
@@ -9,6 +10,7 @@
   [Rpcs3InternalBridgePlugin registerWithRegistrar:registrar];
   [Rpcs3JitBridgePlugin registerWithRegistrar:registrar];
   [Rpcs3RuntimeTuningPlugin registerWithRegistrar:registrar];
+  [Rpcs3DocumentPickerPlugin registerWithRegistrar:registrar];
 }
 
 @end
