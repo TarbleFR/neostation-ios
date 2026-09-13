a93e7e91c7e6e66f8107227592d040c99b611217
Tarble FR <f9wynyv9qy@privaterelay.appleid.com>
2026-09-13 18:14:38 +0200

    Align RPCS3 build packaging with Build 259

--- build-utils/build_rpcs3_embedded_core.sh
+++ build-utils/build_rpcs3_embedded_core.sh
@@ -52,9 +52,9 @@ ccache --max-size=3G >/dev/null
 # These values are consumed by all subsequent workflow steps.
 if [[ -n "${GITHUB_ENV:-}" ]]; then
   {
-    echo "BUILD_NUMBER=258"
-    echo "IPA_NAME=NeoStation-iOS-Build-258"
-    echo "ARTIFACT_NAME=NeoStation-iOS-Build-258"
+    echo "BUILD_NUMBER=259"
+    echo "IPA_NAME=NeoStation-iOS-Build-259-Single-IPA-VPN"
+    echo "ARTIFACT_NAME=NeoStation-iOS-Build-259-Single-IPA-VPN"
   } >> "$GITHUB_ENV"
 fi
 
