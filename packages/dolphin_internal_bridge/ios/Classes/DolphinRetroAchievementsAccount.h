cc0d04cd9667131c857f3930b568867386960f98
Tarble FR <f9wynyv9qy@privaterelay.appleid.com>
2026-09-17 04:45:44 -0700

    fix(dolphin): define dedicated RetroAchievements account flow [skip ci]

---
+++ packages/dolphin_internal_bridge/ios/Classes/DolphinRetroAchievementsAccount.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+// Emulator login tokens are separate from NeoStation's dashboard Web API key.
+@interface DolphinRetroAchievementsAccount : UIViewController <NSURLSessionTaskDelegate>
++ (nullable NSDictionary<NSString*, NSString*>*)credentials;
++ (void)beginSession;
++ (BOOL)hasPendingChanges;
++ (nullable NSURLRequest*)loginRequestForUsername:(NSString*)username password:(NSString*)password;
++ (nullable NSDictionary<NSString*, NSString*>*)credentialsFromResponse:(NSData*)data;
+- (instancetype)initWithLabels:(NSDictionary<NSString*, NSString*>*)labels;
+@end
+NS_ASSUME_NONNULL_END
