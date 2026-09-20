9ce232d27845f616cc9e86fa640bc96f853d3955
Tarble FR <f9wynyv9qy@privaterelay.appleid.com>
2026-09-20 06:44:00 -0700

    ui(rpcs3): expose touch-controls visibility to session menu

--- packages/rpcs3_internal_bridge/ios/Classes/RPCS3GameInputController.h
+++ packages/rpcs3_internal_bridge/ios/Classes/RPCS3GameInputController.h
@@ -15,6 +15,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (void)start;
 - (void)stop;
+@property(nonatomic, assign, getter=isTouchControlsEnabled) BOOL touchControlsEnabled;
 - (void)layoutControlsInBounds:(CGRect)bounds safeAreaInsets:(UIEdgeInsets)insets;
 
 @end
